83cfe51dff04045b864f8a7f69136074457eddd5
jagu-ms <mohamedseddiki20@gmail.com>
2021-10-27 17:57:38 +0200

    ...

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -7,11 +7,22 @@ exports.handler =  (req, res) => {
     const {post, type} = req.body
     Post.like(post, {user, type})
     .then(post => {
-      if(!post) throw createError(404);
-      res.status(200).json()
+        if(!post) throw createError(404);
+        res.status(200).json()
     })
     .catch(next);
 } 
+
+exports.unrepeatable = (req, res) => {
+    const user = req.user.id
+    const {id} = req.body;
+    const post = await Post.findOne({_id: question, parent: null})
+    if (post?.user != user) return res.status(403).json()
+
+    await Post.updateMany({parent: question}, {"answer.accepted": false})
+    await Post.updateMany({parent: question, _id: answer}, {"answer.accepted": true})
+    res.status(200).json()
+}
 
 exports.create = (req, res, next) => {
     let data = {
